204659e6722b0b385537333cdf77964bd6f46ca4
Samatqyzy <40865688+znamazbayeva@users.noreply.github.com>
2022-05-12 22:40:50 +0600

    Time: 63 ms (28.79%), Space: 36.5 MB (85.83%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -3,11 +3,12 @@ public:
     int longestSubarray(vector<int>& nums) {
         int j = 0, i = 0, k = 1, max= 0;
         for(; i < nums.size(); i++) {
-            if(!k && nums[i] == 0) {
-                while(nums[j++] != 0 ){}
-                ++k;
-            }
             if(nums[i] == 0) k--;
+            if(k < 0) {
+                if(nums[j] == 0) k++;
+                ++j;
+            }
+            
             max = max > i - j ? max : i-j;
         }
         return max;
