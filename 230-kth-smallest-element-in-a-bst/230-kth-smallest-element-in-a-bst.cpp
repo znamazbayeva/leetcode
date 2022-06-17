0056666b1460cd1842da5d574b4956222379ba4f
Samatqyzy <40865688+znamazbayeva@users.noreply.github.com>
2022-06-17 20:02:13 +0600

    Time: 24 ms (40.86%), Space: 24.1 MB (87.92%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -11,20 +11,36 @@
  */
 class Solution {
 public:
+    int cnt = 0;
+    int val = 0;
+    void helper(TreeNode* root, int k) 
+    {
+        if(!root) return;
+        kthSmallest(root->left, k);
+        cnt++;
+        if(k == cnt) {val =  root->val; return;}
+        kthSmallest(root->right, k);
+    }
     int kthSmallest(TreeNode* root, int k) {
-        stack<TreeNode*> occ;
-        while(root || !occ.empty()) {
-            while(root) {
-                occ.push(root);
-                root = root->left;
-            }
-            root = occ.top();
-            if(k==1) return root->val;
-            k--;
-            occ.pop();
-            root = root->right;
-        }
+        helper(root, k);
+        return val;
+        // Solution 1. Using DFS and stack
+        // Time O(n)
+        // Space O(1)? or O(n)
+        // stack<TreeNode*> occ;
+        // while(root || !occ.empty()) {
+        //     while(root) {
+        //         occ.push(root);
+        //         root = root->left;
+        //     }
+        //     root = occ.top();
+        //     if(k==1) return root->val;
+        //     k--;
+        //     occ.pop();
+        //     root = root->right;
+        // }
+        // return -1;
         
-        return -1;
+        
     }
 };
