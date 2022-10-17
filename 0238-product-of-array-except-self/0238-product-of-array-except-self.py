db3b1e0393eaad50c51748912cd3cc6fb90bff7e
Samatqyzy <40865688+znamazbayeva@users.noreply.github.com>
2022-10-17 15:00:05 +0600

    Time: 669 ms (13.36%), Space: 21.3 MB (58.74%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -10,15 +10,34 @@ class Solution:
         #             mult *= nums[j]
         #     ans[i] = mult
         # return ans
-        n = len(nums)
-        prefix, suffix = [0]*n, [0]*n
-        prefix[0], suffix[n-1] = nums[0], nums[n-1]
-        ans = [0]*n
+        
+        
+        # with suffix and prefix
+        # n = len(nums)
+        # prefix, suffix = [0]*n, [0]*n
+        # prefix[0], suffix[n-1] = nums[0], nums[n-1]
+        # ans = [0]*n
+        # for i in range(1, n):
+        #     prefix[i] = prefix[i-1]*nums[i]
+        # for i in range(n-2, -1, -1):
+        #     suffix[i] = suffix[i+1]*nums[i]
+        # print(prefix, suffix)
+        # for i in range(0, n):
+        #     ans[i] = (prefix[i-1] if i > 0 else 1) * (suffix[i+1] if i < n-1 else 1)
+        # return ans
+        
+        # using accumulate
+        # prefix = list(accumulate(nums, mul))
+        n, postfix = len(nums), 1
+        ans = [1]*n
         for i in range(1, n):
-            prefix[i] = prefix[i-1]*nums[i]
-        for i in range(n-2, -1, -1):
-            suffix[i] = suffix[i+1]*nums[i]
-        print(prefix, suffix)
-        for i in range(0, n):
-            ans[i] = (prefix[i-1] if i > 0 else 1) * (suffix[i+1] if i < n-1 else 1)
+            ans[i] = nums[i-1]*ans[i-1]
+        for i in range(n-1, -1, -1):
+            ans[i] *= postfix
+            postfix*=nums[i]
         return ans
+        # [1,2,3,4] prefix = 1
+        # [1,1,2,6] postfix = 4
+        # []
+        # [1,2] prefix = 2
+        # [1,2,6] prefix = 6
