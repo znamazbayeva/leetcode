63bd3e5a24bca15703c7932d791e1d7d4892e35e
Samatqyzy <40865688+znamazbayeva@users.noreply.github.com>
2022-10-18 23:17:11 +0600

    Time: 65 ms (24.80%), Space: 13.9 MB (7.89%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,9 +1,14 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
+        # tc -> O(logn), sc -> O(1)
         numberOf1s = 0
-        n1 = 32
-        while n1:
-            numberOf1s = numberOf1s + 1 if n & 1 else numberOf1s
-            n1 -= 1
-            n >>= 1
+        while n:
+            numberOf1s += 1
+            n = n&(n-1) # unset the rightmost bit
+
+        # n1 = 32
+        # while n1:
+        #     numberOf1s = numberOf1s + 1 if n & 1 else numberOf1s
+        #     n1 -= 1
+        #     n >>= 1
         return numberOf1s
