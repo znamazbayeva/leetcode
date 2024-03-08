57bbf60d8f677d3d03d2f53c7773e10f8f80c31a
Samatqyzy <40865688+znamazbayeva@users.noreply.github.com>
2024-03-08 11:23:56 +0100

    Time: 11 ms, Memory: 12 MB - LeetHub

---
+++ pascals-triangle-ii/pascals-triangle-ii.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        arr = [[0]*n for n in range(1,35)]
+        def helper(row, col):
+            if row >= 0 and col >= 0 and col <=row and arr[row][col] != 0:
+                return arr[row][col]
+            if row == 0:
+                return 1
+            if col == 0 or col == row:
+                return 1
+            elif col > row or col < 0:
+                return 0
+            ans = helper(row-1, col-1) + helper(row-1, col)
+            arr[row][col] = ans
+            return ans
+        res = []
+        for i in range(rowIndex + 1):
+            res.append(helper(rowIndex, i))
+        return res
